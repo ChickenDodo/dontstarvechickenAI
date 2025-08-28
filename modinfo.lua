e64007698c04b7c2afe24a011713aba775fcc9ba
ChickenDodo <adrianjaakwee@gmail.com>
2025-08-29 03:40:42 +1000

    Updating Modinfo for uploading

--- modinfo.lua
+++ modinfo.lua
@@ -1,11 +1,11 @@
 --The name of the mod displayed in the 'mods' screen.
-name = "Stalking Chicken"
+name = "Chicken AI Example"
 
 --A description of the mod.
-description = "This tutorial shows you how to have your creature spawn throughout the world!"
+description = "This mod is a demonstration of custom-made behaviours (with many liberties taken and forum-searching), utilising Cheerio of Klei Entertainment's Creature Mod Tutorial series assets. Don't expect much more other than a demonstration for a project."
 
 --Who wrote this awesome mod?
-author = "ChickenDodo"
+author = "ChickenDodo & Cheerio of Klei Entertainment"
 
 --A version number so you can ask people if they are running an old version of your mod.
 version = "1.0"
